b0db1f28930d9aed4652d59be7cbf8fcfd655975
ThanyaphornSaetao347 <142294145+ThanyaphornSaetao347@users.noreply.github.com>
2025-10-03 11:47:51 +0700

    แก้ export excel

--- src/export-excel/export-excel.controller.ts
+++ src/export-excel/export-excel.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, Res, Body } from '@nestjs/common';
+import { Controller, Post, Res, Body, UseGuards } from '@nestjs/common';
 import { ExportExcelService } from './export-excel.service';
 import { Response } from 'express';
+import { JwtAuthGuard } from '../auth/jwt_auth.guard';
 
 @Controller('api')
 export class ExportExcelController {
   constructor(private readonly exportExcelService: ExportExcelService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('export-excel')
   async exportTickets(@Res() res: Response, @Body() filter: any) {
     return this.exportExcelService.exportTickets(res, filter);
